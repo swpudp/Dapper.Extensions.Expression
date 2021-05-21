e4bc17c49b4aedbf381802138e21ecad4b7555ad
dingpin <dingpin@yzw.com>
2021-05-21 17:40:22 +0800

    test3

--- Dapper.Expression.Preview1Tests/BaseTest.cs
+++ Dapper.Expression.Preview1Tests/BaseTest.cs
@@ -191,6 +191,7 @@ namespace Dapper.Extensions.Expression.UnitTests
             Trace.WriteLine(nameof(Test) + "16点58分");
             Trace.WriteLine(nameof(Test) + "17点04分");
             Trace.WriteLine(nameof(Test) + "17点38分");
+            Trace.WriteLine(nameof(Test) + "17点40分");
         }
     }
 }
